3f9c367e4518e103199dc8f8e439e2a12483a4d1
YeuGmiko <yeu.gmiko@qq.com>
2024-12-13 11:09:49 +0800

    update(style): 修改文本缩进

--- src/renderer/src/views/layout/myLove/index.vue
+++ src/renderer/src/views/layout/myLove/index.vue
@@ -1,29 +1,38 @@
 <script setup lang="ts">
-import { tabs } from './config'
-import { onMounted } from 'vue'
+import { computed, onMounted } from 'vue'
+import { useSongStore } from '@/store/song'
+import { storeToRefs } from 'pinia'
+
+const playListName = 'loved-list'
+
+const songStore = useSongStore()
+const { songs } = storeToRefs(songStore)
+
+const lovedSongs = computed<App.BaseInfo.Song[]>(() => songs.value.filter((item) => item.isLoved))
 onMounted(() => {
-  console.log('MyLove Page Mounted')
+    console.log('MyLove Page Mounted')
 })
 </script>
 
 <template>
-  <div class="my-love">
-    <h1 class="title">我喜欢</h1>
-    <NTabs class="tabs">
-      <NTabPane v-for="tab in tabs" :key="tab.name" :name="tab.name" :tab="tab.label">
-        <component :is="tab.component"></component>
-      </NTabPane>
-    </NTabs>
-  </div>
+    <div class="my-love">
+        <h1 class="title">我喜欢</h1>
+        <!--    <NTabs class="tabs">-->
+        <!--      <NTabPane v-for="tab in tabs" :key="tab.name" :name="tab.name" :tab="tab.label">-->
+        <!--        <component :is="tab.component"></component>-->
+        <!--      </NTabPane>-->
+        <!--    </NTabs>-->
+        <SongList :songs="lovedSongs" :play-list-name="playListName"></SongList>
+    </div>
 </template>
 
 <style scoped lang="scss">
 .my-love {
-  .title {
-    margin: 0;
-  }
-  .tabs {
-    margin-top: 10px;
-  }
+    .title {
+        margin: 0;
+    }
+    .tabs {
+        margin-top: 10px;
+    }
 }
 </style>
